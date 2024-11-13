637e14a9868b127c93a7ca68c6b94ab08f5f5e84
albertomorini <99morini@gmail.com>
2024-11-13 08:39:05 +0100

    solved another error 400 bad request

--- uTubeDwn.py
+++ uTubeDwn.py
@@ -1,11 +1,60 @@
-from pytubefix import YouTube
+# from pytubefix import YouTube
+# pip install yt-dlp
 import moviepy.editor as mp
 import os
 import re
-from pytube.innertube import _default_clients
+# from pytube.innertube import _default_clients
 
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
+
+
+import ssl
+from pytube import YouTube
+from pytube import request
+from pytube import extract
+from pytube.innertube import _default_clients
+from pytube.exceptions import RegexMatchError
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
+
+import pytube, re
+def patched_get_throttling_function_name(js: str) -> str:
+    function_patterns = [
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+    ]
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+                        nfunc=re.escape(function_match.group(1))),
+                    js
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(
+        caller="get_throttling_function_name", pattern="multiple"
+    )
+
+ssl._create_default_https_context = ssl._create_unverified_context
+pytube.cipher.get_throttling_function_name = patched_get_throttling_function_name
 
 
 ########################################
